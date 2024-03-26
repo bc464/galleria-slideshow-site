45c860ae2fa2c989e30e5d7b488a72883db48c5d
bc464 <bclaassen9@gmail.com>
2024-03-26 17:58:21 +0200

    more work on starry night page but responsiveness

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const boxContents = document.querySelectorAll(".box-content");
-const slideshowPage = document.getElementsByClassName("slideshow-page");
+const slideshowPage = document.querySelector(".slideshow-page");
 const module = document.querySelector("module");
 
 boxContents.forEach((box) => {
@@ -14,4 +14,18 @@ boxContents.forEach((box) => {
       window.location.href = "starry-night.html";
     }
   });
+});
+
+const viewImage = document.querySelector(".viewImageLink");
+const moduleContainer = document.querySelector(".module-container");
+
+const closeBtn = document.querySelector(".btn-close");
+
+viewImage.addEventListener("click", () => {
+  slideshowPage.style.opacity = "0.1";
+  moduleContainer.style.display = "block";
+});
+closeBtn.addEventListener("click", () => {
+  moduleContainer.style.display = "none";
+  slideshowPage.style.opacity = "1";
 });
